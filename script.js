2b858018755539c68c221a60b52118868dce8e0e
Juan S Silva <juanse909@hotmail.com>
2024-04-02 08:12:23 -0500

    taller-clase-19

---
+++ script.js
@@ -0,0 +1,31 @@
+const render = () => {
+    const digimones = [
+        "https://digimon.shadowsmith.com/img/koromon.jpg",
+        "https://digimon.shadowsmith.com/img/tsunomon.jpg",
+        "https://digimon.shadowsmith.com/img/yokomon.jpg",
+        "https://digimon.shadowsmith.com/img/motimon.jpg",
+        "https://digimon.shadowsmith.com/img/patamon.jpg",
+        "https://digimon.shadowsmith.com/img/kuwagamon.jpg",
+        "https://digimon.shadowsmith.com/img/greymon.jpg",
+        "https://digimon.shadowsmith.com/img/tanemon.jpg",
+        ];
+    
+        const digimonesContenedor = document.querySelector(".digimones");
+    
+        for(const digimon of digimones){
+            const img = document.createElement("img");
+            img.setAttribute("src", digimon);
+            img.setAttribute("alt", digimon);
+    
+            img.classList.add("imagen");
+    
+            digimonesContenedor.appendChild(img);
+
+            img.addEventListener("click", () => {
+                alert(digimon);
+            });
+        };
+        
+};
+
+document.addEventListener("DOMContentLoaded", render)
